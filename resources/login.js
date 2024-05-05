0863616c69d2a7f6bc1289e0927e4dc5444a2f7e
Alex Denisova <alexadenisova@gmail.com>
2024-05-05 19:00:14 +0300

    added demo

--- resources/login.js
+++ resources/login.js
@@ -2,18 +2,9 @@ import { deleteLogin, postLogin } from "./backend/login.js";
 import { postUser } from "./backend/users.js";
 import { showMessage } from "./utils.js";
 
-var clicked;
-
 window.login = async function (username_id, password_id) {
   const username = document.getElementById(username_id).value;
   const password = document.getElementById(password_id).value;
-  if (clicked == "Create") {
-    const response = await postUser(username, password);
-    if (!response.ok) {
-      showMessage("Could not create account", false);
-      return false;
-    }
-  }
   const response = await postLogin(username, password);
   if (!response.ok) {
     showMessage("Wrong username or password", false);
@@ -21,8 +12,23 @@ window.login = async function (username_id, password_id) {
   return false;
 }
 
-window.clickSubmit = function (name) {
-  clicked = name;
+window.createAccount = async function (username_id, password_id) {
+  const username = document.getElementById(username_id).value;
+  const password = document.getElementById(password_id).value;
+  const response = await postUser(username, password);
+  if (!response.ok) {
+    showMessage("Could not create account", false);
+    return false;
+  }
+  return await login(username_id, password_id);
+}
+
+window.demo = async function () {
+  const response = await postLogin("demo");
+  if (!response.ok) {
+    showMessage("Error logging in as demo", false);
+  }
+  return false;
 }
 
 window.logout = async function () {
