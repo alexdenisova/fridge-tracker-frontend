1d0841bab2206a7cd0ee54e7aa0d28992618f7b9
Alex Denisova <alexadenisova@gmail.com>
2024-05-12 00:56:38 +0300

    testing kaspersky

--- resources/pantry/add.js
+++ resources/pantry/add.js
@@ -20,24 +20,24 @@ window.addPantryItemButton = function (add_id) {
   }
 }
 
-// function addForm() {
-//   const div = document.createElement("div");
-//   div.setAttribute("class", "form");
-//   div.setAttribute("id", ADD_ID);
-//   const select = unitOptions();
-//   div.innerHTML = `
-//     <div class="form-heading">Provide pantry item information</div>
-//     <form onsubmit="submitPantryItem(); return false;">
-//       <label for="ingredient_name"><span>Ingredient Name<span class="required">*</span></span><input type="text" class="input-field" id="ingredient_name" name="ingredient_name"></label>
-//       <label for="purchase_date"><span>Purchase Date</span><input type="date" class="input-field" id="purchase_date" name="purchase_date" placeholder="YYYY-MM-DD"></label>
-//       <label for="expiration_date"><span>Expiraton Date</span><input type="date" class="input-field" id="expiration_date" name="expiration_date" placeholder="YYYY-MM-DD"></label>
-//       <label for="amount"><span>Amount</span><input type="text" class="input-field" id="amount" name="amount" style="width:30%;">${select.outerHTML}</label>
-//       <label for="running_low"><span>Running low at</span><input type="text" class="input-field" id="running_low" name="running_low" style="width:30%;"></label>
-//       <label for="essential"><span>Essential</span><input type="checkbox" class="input-checkbox" id="essential"></label>
-//       <label><span> </span><input type="submit" value="Submit"></label>
-//     </form>`;
-//   main.appendChild(div);
-// }
+function addForm() {
+  const div = document.createElement("div");
+  div.setAttribute("class", "form");
+  div.setAttribute("id", ADD_ID);
+  const select = unitOptions();
+  div.innerHTML = `
+    <div class="form-heading">Provide pantry item information</div>
+    <form onsubmit="submitPantryItem(); return false;">
+      <label for="ingredient_name"><span>Ingredient Name<span class="required">*</span></span><input type="text" class="input-field" id="ingredient_name" name="ingredient_name"></label>
+      <label for="purchase_date"><span>Purchase Date</span><input type="date" class="input-field" id="purchase_date" name="purchase_date" placeholder="YYYY-MM-DD"></label>
+      <label for="expiration_date"><span>Expiraton Date</span><input type="date" class="input-field" id="expiration_date" name="expiration_date" placeholder="YYYY-MM-DD"></label>
+      <label for="amount"><span>Amount</span><input type="text" class="input-field" id="amount" name="amount" style="width:30%;">${select.outerHTML}</label>
+      <label for="running_low"><span>Running low at</span><input type="text" class="input-field" id="running_low" name="running_low" style="width:30%;"></label>
+      <label for="essential"><span>Essential</span><input type="checkbox" class="input-checkbox" id="essential"></label>
+      <label><span> </span><input type="submit" value="Submit"></label>
+    </form>`;
+  main.appendChild(div);
+}
 
 // window.submitPantryItem = async function () {
 //   const name = document.getElementById('ingredient_name').value;
